af43a7600d4e8d0711b307489af662d959fefc76
HiFriendu <korimillisrinivas@2002gmail.com>
2023-11-08 00:25:56 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def silk(request):
+    return HttpResponse ("i miss you mia i like your wonderfull acting"),
